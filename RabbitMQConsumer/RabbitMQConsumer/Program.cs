4e31d59b627ebf81b9f93acf6fbaca96861836b4
Yagmur Tasci <yagmur_tascii@hotmail.com>
2024-08-01 18:18:41 +0300

    RabbitMQ Consumer created

---
+++ RabbitMQConsumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQConsumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localHost"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "queue", exclusive: false, autoDelete: false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var body = eventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+            channel.BasicConsume(queue: "queue", autoAck: true, consumer: consumer);
+            Console.ReadKey();
+        }
+    }
+}
+
